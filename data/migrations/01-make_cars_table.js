13a6d920f472cd41dd87f8dc14c38a065b24837b
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-19 12:28:49 -0700

    up and down created

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,24 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    //id
+    tbl.increments()
+    //vin
+    tbl.string('vin', 17).notNullable().unique()
+    //make
+    tbl.string('make', 128).notNullable()
+    //model
+    tbl.string('model', 128).notNullable()
+    //mileage
+    tbl.numeric('mileage').unsigned().notNullable()
+    //title
+    tbl.string('title', 128)
+    //transmission
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
