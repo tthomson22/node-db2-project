b3a07997098fd1823d11ae059f91c52f1db91146
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-19 12:16:30 -0700

    added seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'Toyota',
+        model: 'Prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '1111111111111',
+        make: 'Honda',
+        model: 'Civic',
+        mileage: 107000,
+        title: 'clean',
+    },
+    {
+        vin: '1111111111111',
+        make: 'Tesla',
+        model: 'Y',
+        mileage: 1000,
+        title: 'new',
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
